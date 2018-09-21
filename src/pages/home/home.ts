9ce4bed12d26d96e24ec87b4b8629b7939b2df9d
Doan Quoc Cuong <cuong.dq@mobifone.vn>
2018-09-21 15:02:16 +0700

    dich vu vi tri trong ionic-anguala

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  loc: Geolocation;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) { }
+
+  ngOnInit() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log('Toa do lay duoc la: ' +  resp.coords.latitude + ',' + resp.coords.longitude);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+    let watch = this.geolocation.watchPosition();
+
+    watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+      console.log('Data watch: ' +  data.coords.latitude + ',' + data.coords.longitude);
+    });
 
   }
+
+
 
 }
