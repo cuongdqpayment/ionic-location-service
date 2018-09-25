3fd47dd514f18e90da47309b2fe1e1b319831d02
Doan Quoc Cuong <cuong.dq@mobifone.vn>
2018-09-25 07:30:46 +0700

    View dep

---
+++ src/models/Location.model.ts
@@ -0,0 +1,11 @@
+export class Location {
+    lat:number;
+    lon:number;
+    heading: number;
+    speed:number;
+    accuracy:number;
+    altitude:number;
+    altitudeAccuracy:number;
+    timestamp: number;
+    count:number = 0;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LocationService } from '../../services/location-service';
 import { Subscription } from 'rxjs/Subscription';
+import { Location } from '../../models/Location.model';
 
 @Component({
   selector: 'page-home',
@@ -10,42 +11,63 @@ import { Subscription } from 'rxjs/Subscription';
 export class HomePage {
   
   locTracked:Subscription;
+  locCur:Location = new Location();
+  locMove:Location = new Location();
+  
+
 
   constructor(public navCtrl: NavController, private loc: LocationService) { }
 
   //ham nay se chay sau khi contructor khoi tao xong
   ngOnInit() {
     //lay vi tri theo dich vu vi tri da khai bao
-    this.getLocation();
-    //theo doi thay doi vi tri cua thiet bi
-    this.locTracked = this.trackingLocation();
+    // this.getLocation();
+    // //theo doi thay doi vi tri cua thiet bi
+    // this.locTracked = this.trackingLocation();
 
-    setTimeout(()=>{
-      this.stopTracking()
-      },10000)
+    // setTimeout(()=>{
+    //   this.stopTracking()
+    //   },10000)
+    this.locCur = new Location();
+    this.locMove = new Location();
   }
 
   getLocation() {
     this.loc.getCurrentLoc()
     .then(pos => {
-      console.log("loc="+pos.coords.latitude+","+pos.coords.longitude);
+      //console.log("loc="+pos.coords.latitude+","+pos.coords.longitude);
+      this.locCur.accuracy = pos.coords.accuracy;
+      this.locCur.lat = pos.coords.latitude;
+      this.locCur.lon = pos.coords.longitude;
+      this.locCur.altitude = pos.coords.altitude;
+      this.locCur.altitudeAccuracy = pos.coords.altitudeAccuracy;
+      this.locCur.heading = pos.coords.heading;
+      this.locCur.speed = pos.coords.speed;
+      //this.locCur.timestamp = pos.timestamp;
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log('Loi: ' + err))
   }
 
   trackingLocation(){
    return this.loc.trackingLoc()
-    .subscribe(p => {
-      console.log("vi tri thay doi="+p.coords.latitude+","+p.coords.longitude);
-    },err=> console.log("Err: " + err))
+    .subscribe(pos => {
+      //console.log("vi tri thay doi="+p.coords.latitude+","+p.coords.longitude);
+      this.locMove.count++;
+      this.locMove.accuracy = pos.coords.accuracy;
+      this.locMove.lat = pos.coords.latitude;
+      this.locMove.lon = pos.coords.longitude;
+      this.locMove.altitude = pos.coords.altitude;
+      this.locMove.altitudeAccuracy = pos.coords.altitudeAccuracy;
+      this.locMove.heading = pos.coords.heading;
+      this.locMove.speed = pos.coords.speed;
+      //this.locMove.timestamp = pos.timestamp;
+      
+    },err=> console.log("Loi Tracking: " + err))
   }
 
   stopTracking(){
     console.log("stop tracking!");
     this.locTracked.unsubscribe();
   }
-
-
-
 
 }
